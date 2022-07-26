a347a6a6a50f34ccf55435c0fbfae8719020bab4
AlayaElla <515451867@qq.com>
2022-07-26 20:02:29 +0800

    Add files via upload

---
+++ ChangeWordpressPost.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from wordpress_xmlrpc import Client
+from wordpress_xmlrpc import WordPressPost
+from wordpress_xmlrpc.compat import xmlrpc_client
+from wordpress_xmlrpc.methods import posts,media
+import re
+import datetime
+import pytz
+
+#配置
+usr = "usr"
+password = 'pwd'
+address = 'http://127.0.0.1'
+
+plantDic = {
+            "44":"无尽夏（大）", 
+            "58":"无尽夏（小）", 
+            "62":"紫叶酢浆草", 
+            "64":"乒乓菊", 
+            "66":"无尽夏扦插苗", 
+            "68":"香雪兰种子1", 
+            "70":"香雪兰种子2", 
+        }
+
+actionDic = {
+            "1":"浇水", 
+            "2":"施肥", 
+            "3":"打药", 
+        }
+
+#检查花的id是否存在并且获取花名
+def getFlower(id):
+        try:
+                return plantDic[id]
+        except:
+                return
+
+#检查动作id是否合法并且获取动作
+def getAction(id):
+        try:
+                return actionDic[id]
+        except:
+                return
+
+
+#更新内容函数
+def updatePlant(value,_post):
+        nowtime = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+        nowtimeStr = nowtime.strftime('%Y-%m-%d')
+
+        pattern = '<li>'+value+'：(.*?)</li>'
+        repl = '<li>'+value+'：'+nowtimeStr+'</li>'
+        _post.content = re.sub(pattern, repl, _post.content, count=0, flags=0)
+        return _post
+
+#比较时间
+def compare_time(time1,time2):
+        d1 = datetime.datetime.strptime(time1, '%Y-%m-%d')
+        d2 = datetime.datetime.strptime(time2, '%Y-%m-%d')
+        delta = d1 - d2
+        return delta.days
+
+#更新标题函数
+def updatePlantTitle(_post):
+        nowtime = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+        nowtimeStr = nowtime.strftime('%Y-%m-%d')
+
+        #获取标题
+        title = _post.title
+        pattern = '(.*)：(.*)'
+        title0 = re.search(pattern,title).group(1) 
+        #获取上次浇水时间
+        water_pattern = '<li>浇水：(.*?)</li>'
+        last_water_day = re.search(water_pattern,_post.content).group(1).split(" ")[0]
+        day = compare_time(nowtimeStr,last_water_day)
+        if(day!=0):
+                title1 = "上次浇水"+day.__str__()+"天前"
+        else:
+                title1 = "今天刚浇了水"
+        repl = title0+'：'+title1
+        _post.title = re.sub(pattern, repl, _post.title, count=0, flags=0)
+        return _post
+
+#根据id获取文章
+def getWordpressPost(client,plantName):
+        post = client.call(posts.GetPost(plantName))
+        return post
+
+#post = updatePlant("浇水",post)
+#post = updatePlantTitle(post)
+
+def getclient():
+        return Client(address+"/xmlrpc.php",usr,password)
+
+def EditPost(client,plantName,_post):
+        return client.call(posts.EditPost(plantName,_post))
---
+++ PlantCareServer.py
@@ -0,0 +1,61 @@
+from flask import Flask, redirect, url_for, request
+import ChangeWordpressPost
+import datetime
+import pytz
+import hashlib
+
+passwd = "HELLO"
+def compareToken(passwd,usr_token):
+    nowtime = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
+    nowtimeStr = nowtime.strftime('%Y-%m-%d %H:%M')
+    pre_token = nowtimeStr+passwd
+    token = hashlib.sha256(pre_token.encode('utf-8')).hexdigest()
+    print(pre_token,token,usr_token)
+    if(token == usr_token):
+        return True
+    else:
+        return False
+
+app = Flask(__name__)
+
+@app.route('/plantcare/',methods = ['POST'])
+def plantcare_post():
+   if request.method == 'POST':
+        text_content = ""
+
+        flowerName = ChangeWordpressPost.getFlower(request.json["flower"])
+        actionName = ChangeWordpressPost.getAction(request.json["action"])
+
+        #print(request.json["flower"],flowerName)
+        #print(request.json["action"],actionName)
+
+        #身份验证
+        usr_token = request.json["token"]
+        if(not compareToken(passwd,usr_token)):
+            return "🔒🔒🚫🚫权限错误!"
+
+        if(flowerName != None and actionName != None):
+            client = ChangeWordpressPost.getclient()
+            #获取文章
+            post = ChangeWordpressPost.getWordpressPost(client,request.json["flower"])
+
+            #更新文档动作日期
+            post = ChangeWordpressPost.updatePlant(actionName,post)
+            if(request.json["action"] == "1"): #如果是浇水则更新标题
+                post = ChangeWordpressPost.updatePlantTitle(post)
+
+            #更新文档并且输出log
+            if(ChangeWordpressPost.EditPost(client,request.json["flower"],post)):
+                text_content = "🪴🪴🥰🥰"+flowerName+actionName+"完成!"
+                #print(flowerName+" "+ actionName+"更新完成！")
+            else:
+                text_content = "🪴🪴😱😱"+flowerName+actionName+"失败!"
+                #print(flowerName+" "+ actionName+"更新失败！")
+        else:
+                text_content = "🤕🤕🤕🤕指令错误!"
+                #print("指令错误！")
+
+        return text_content
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=123)
